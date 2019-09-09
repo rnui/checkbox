63547751be67e206ee99482a284b1ab243654324
范文华 <531362022@qq.com>
2019-09-09 17:28:44 +0800

    chore: Update icon version

--- src/iconfont/Icon.tsx
+++ src/iconfont/Icon.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable */
 
 import React, { FunctionComponent } from 'react';
-import {ViewProps} from 'react-native';
+import { ViewProps } from 'react-native';
 import { Svg, GProps, Path } from 'react-native-svg';
 
 
@@ -24,7 +24,7 @@ const Icon: FunctionComponent<Props> = ({ color, name, size, ...rest }) => {
         <Svg viewBox="0 0 1024 1024" width={size} height={size} {...rest}>
           <Path
             d="M96 64h832a32 32 0 0 1 32 32v832a32 32 0 0 1-32 32H96a32 32 0 0 1-32-32V96a32 32 0 0 1 32-32z m648.64 244.032l-310.368 331.296-157.696-132.48a32 32 0 1 0-41.152 48.992l180.896 152a32 32 0 0 0 43.936-2.624L791.36 351.776a32 32 0 1 0-46.72-43.744z"
-            fill={color ? typeof color === 'string' && color || color[0] || '#333333' : '#333333'}
+            fill={getIconColor(color, 0, '#333333')}
           />
         </Svg>
       );
@@ -33,7 +33,7 @@ const Icon: FunctionComponent<Props> = ({ color, name, size, ...rest }) => {
         <Svg viewBox="0 0 1024 1024" width={size} height={size} {...rest}>
           <Path
             d="M896 64H128c-35.296 0-64 28.704-64 64v768c0 35.296 28.704 64 64 64h768c35.296 0 64-28.704 64-64V128c0-35.296-28.704-64-64-64zM128 896V128h768l0.064 768H128z"
-            fill={color ? typeof color === 'string' && color || color[0] || '#333333' : '#333333'}
+            fill={getIconColor(color, 0, '#333333')}
           />
         </Svg>
       );
@@ -42,11 +42,11 @@ const Icon: FunctionComponent<Props> = ({ color, name, size, ...rest }) => {
         <Svg viewBox="0 0 1024 1024" width={size} height={size} {...rest}>
           <Path
             d="M896 64H128c-35.296 0-64 28.704-64 64v768c0 35.296 28.704 64 64 64h768c35.296 0 64-28.704 64-64V128c0-35.296-28.704-64-64-64zM128 896V128h768l0.064 768H128z"
-            fill={color ? typeof color === 'string' && color || color[0] || '#333333' : '#333333'}
+            fill={getIconColor(color, 0, '#333333')}
           />
           <Path
             d="M744.64 308.032l-310.368 331.296-157.696-132.48a32 32 0 0 0-41.184 49.024l180.896 152a32 32 0 0 0 43.936-2.624L791.36 351.776a32 32 0 1 0-46.72-43.744z"
-            fill={color ? typeof color === 'string' && color || color[1] || '#333333' : '#333333'}
+            fill={getIconColor(color, 1, '#333333')}
           />
         </Svg>
       );
@@ -60,7 +60,15 @@ Icon.defaultProps = {
   size: 18,
 };
 
-export default Icon;
+const getIconColor = (color: string | string[] | undefined, index: number, defaultColor: string) => {
+  return color
+    ? (
+      typeof color === 'string'
+        ? color
+        : color[index] || defaultColor
+    )
+    : defaultColor;
+};
 
-/* eslint-enable */
-/* tslint:enable */
+
+export default Icon;
